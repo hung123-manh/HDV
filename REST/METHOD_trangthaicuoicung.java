898fb3fa4305206a32cda0d455a30155ebaf33b0
Manh Hung <lengocmanhhunh@gmail.com>
2026-06-14 17:32:18 +0700

    Update METHOD_trangthaicuoicung.java

--- REST/METHOD_trangthaicuoicung.java
+++ REST/METHOD_trangthaicuoicung.java
@@ -1,118 +1,176 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.google.gson.*;
+import java.io.*;
+import java.net.*;
 import java.util.HashMap;
 
-public class czdyjrO2 {
+public class main {
 
     public static void main(String[] args) throws Exception {
 
         String studentCode = "B22DCAT134";
-        String qCode = "czdyjrO2";
+        String qCode = "ap6N4TDT";
         String baseUrl = "http://36.50.135.242:2230/api/rest/method";
 
-        String getUrlStr = baseUrl + "?studentCode=" + studentCode + "&qCode=" + qCode;
+        String getUrl =
+                baseUrl
+                + "?studentCode="
+                + studentCode
+                + "&qCode="
+                + qCode;
 
-        URL getUrl = new URL(getUrlStr);
-        HttpURLConnection getConn = (HttpURLConnection) getUrl.openConnection();
+        HttpURLConnection getConn =
+                (HttpURLConnection)
+                        new URL(getUrl)
+                                .openConnection();
+
         getConn.setRequestMethod("GET");
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(getConn.getInputStream()));
-        StringBuilder response = new StringBuilder();
-        String inputLine;
+        BufferedReader in =
+                new BufferedReader(
+                        new InputStreamReader(
+                                getConn.getInputStream()));
 
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        StringBuilder response =
+                new StringBuilder();
+
+        String line;
+
+        while ((line = in.readLine()) != null) {
+            response.append(line);
         }
 
         in.close();
 
-        String jsonStr = response.toString();
+        String jsonStr =
+                response.toString();
 
-        String reqTag = "\"requestId\":\"";
-        int reqStart = jsonStr.indexOf(reqTag) + reqTag.length();
-        int reqEnd = jsonStr.indexOf("\"", reqStart);
-        String requestId = jsonStr.substring(reqStart, reqEnd);
+        System.out.println("GET:");
+        System.out.println(jsonStr);
 
-        String stateTag = "\"initialState\":\"";
-        int stateStart = jsonStr.indexOf(stateTag) + stateTag.length();
-        int stateEnd = jsonStr.indexOf("\"", stateStart);
-        String currentState = jsonStr.substring(stateStart, stateEnd);
+        JsonObject root =
+                JsonParser.parseString(jsonStr)
+                        .getAsJsonObject();
 
-        int eventsStart = jsonStr.indexOf("\"events\"");
-        eventsStart = jsonStr.indexOf("[", eventsStart) + 1;
-        int eventsEnd = jsonStr.indexOf("]", eventsStart);
+        String requestId =
+                root.get("requestId")
+                        .getAsString();
 
-        String eventsStr = jsonStr.substring(eventsStart, eventsEnd);
-        String[] events = eventsStr.replace("\"", "").split(",");
+        JsonObject data =
+                root.getAsJsonObject("data");
 
-        int transStart = jsonStr.indexOf("\"transitions\"");
-        transStart = jsonStr.indexOf("[", transStart) + 1;
-        int transEnd = jsonStr.indexOf("]", transStart);
+        String currentState =
+                data.get("initialState")
+                        .getAsString();
 
-        String transStr = jsonStr.substring(transStart, transEnd);
-        String[] transitions = transStr.replace("\"", "").split(",");
+        JsonArray events =
+                data.getAsJsonArray("events");
 
-        HashMap<String,String> map = new HashMap<>();
+        JsonArray transitions =
+                data.getAsJsonArray("transitions");
 
-        for(int i=0;i+2<transitions.length;i+=3){
-            String from = transitions[i].trim();
-            String event = transitions[i+1].trim();
-            String to = transitions[i+2].trim();
+        HashMap<String, String> map =
+                new HashMap<>();
 
-            map.put(from + "#" + event, to);
+        for (JsonElement e : transitions) {
+
+            JsonObject t =
+                    e.getAsJsonObject();
+
+            String from =
+                    t.get("from")
+                            .getAsString();
+
+            String event =
+                    t.get("event")
+                            .getAsString();
+
+            String to =
+                    t.get("to")
+                            .getAsString();
+
+            map.put(
+                    from + "#" + event,
+                    to
+            );
         }
 
-        for(String event : events){
+        for (JsonElement e : events) {
 
-            event = event.trim();
+            String event =
+                    e.getAsString();
 
-            String key = currentState + "#" + event;
+            String key =
+                    currentState
+                    + "#"
+                    + event;
 
-            if(map.containsKey(key)){
-                currentState = map.get(key);
+            if (map.containsKey(key)) {
+
+                currentState =
+                        map.get(key);
             }
         }
 
-        String answer = currentState;
+        String answer =
+                currentState;
 
-        URL putUrl = new URL(baseUrl + "/" + requestId);
-        HttpURLConnection putConn = (HttpURLConnection) putUrl.openConnection();
+        System.out.println("\nANSWER:");
+        System.out.println(answer);
 
-        putConn.setRequestMethod("PUT");
-        putConn.setRequestProperty("Content-Type", "application/json");
-        putConn.setDoOutput(true);
-
-        String jsonPut = "{"
-                + "\"studentCode\":\"" + studentCode + "\","
-                + "\"qCode\":\"" + qCode + "\","
-                + "\"answer\":\"" + answer + "\""
+        String jsonPut =
+                "{"
+                + "\"studentCode\":\""
+                + studentCode
+                + "\","
+                + "\"qCode\":\""
+                + qCode
+                + "\","
+                + "\"answer\":\""
+                + answer
+                + "\""
                 + "}";
 
-        OutputStream os = putConn.getOutputStream();
-        os.write(jsonPut.getBytes("UTF-8"));
+        HttpURLConnection putConn =
+                (HttpURLConnection)
+                        new URL(
+                                baseUrl
+                                + "/"
+                                + requestId)
+                                .openConnection();
+
+        putConn.setRequestMethod("PUT");
+        putConn.setRequestProperty(
+                "Content-Type",
+                "application/json");
+        putConn.setDoOutput(true);
+
+        OutputStream os =
+                putConn.getOutputStream();
+
+        os.write(
+                jsonPut.getBytes("UTF-8"));
+
         os.flush();
         os.close();
 
-        int responseCode = putConn.getResponseCode();
+        BufferedReader resultReader =
+                new BufferedReader(
+                        new InputStreamReader(
+                                putConn.getInputStream()));
 
-        if(responseCode == HttpURLConnection.HTTP_OK){
+        StringBuilder result =
+                new StringBuilder();
 
-            BufferedReader putIn = new BufferedReader(new InputStreamReader(putConn.getInputStream()));
-            String line;
-            StringBuilder result = new StringBuilder();
+        while ((line =
+                resultReader.readLine()) != null) {
 
-            while((line = putIn.readLine()) != null){
-                result.append(line);
-            }
-
-            putIn.close();
-
-            System.out.println("Answer = " + answer);
-            System.out.println(result.toString());
+            result.append(line);
         }
+
+        resultReader.close();
+
+        System.out.println("\nRESULT:");
+        System.out.println(result.toString());
     }
 }
 //Một dịch vụ REST được triển khai trên server tại URL http://<Exam_IP>:2230/api/rest/method để xử lý các bài toán mô phỏng phương thức xử lý, trạng thái và quan hệ phụ thuộc. Yêu cầu: Viết chương trình tại máy trạm (REST client) để giao tiếp với dịch vụ và thực hiện các công việc sau.
