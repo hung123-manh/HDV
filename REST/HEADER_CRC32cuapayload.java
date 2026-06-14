efb35b3218c8911d6afcdb79d8cbeeb2beb669f2
Manh Hung <lengocmanhhunh@gmail.com>
2026-06-14 15:20:57 +0700

    Create HEADER_CRC32cuapayload.java

---
+++ REST/HEADER_CRC32cuapayload.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.zip.CRC32;
+
+public class nFn8VjtL {
+
+    public static void main(String[] args) throws Exception {
+
+        String studentCode = "B22DCAT134";
+        String qCode = "nFn8VjtL";
+        String baseUrl = "http://36.50.135.242:2230/api/rest/header";
+
+        String getUrlStr = baseUrl + "?studentCode=" + studentCode + "&qCode=" + qCode;
+
+        URL getUrl = new URL(getUrlStr);
+        HttpURLConnection getConn = (HttpURLConnection) getUrl.openConnection();
+        getConn.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(getConn.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String line;
+
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+
+        in.close();
+
+        String jsonStr = response.toString();
+
+        String reqTag = "\"requestId\":\"";
+        int reqStart = jsonStr.indexOf(reqTag) + reqTag.length();
+        int reqEnd = jsonStr.indexOf("\"", reqStart);
+        String requestId = jsonStr.substring(reqStart, reqEnd);
+
+        String payloadTag = "\"payload\":\"";
+        int payloadStart = jsonStr.indexOf(payloadTag) + payloadTag.length();
+        int payloadEnd = jsonStr.indexOf("\"", payloadStart);
+
+        String payload = jsonStr.substring(payloadStart, payloadEnd);
+
+        CRC32 crc32 = new CRC32();
+        crc32.update(payload.getBytes("UTF-8"));
+
+        String answer = Long.toHexString(crc32.getValue());
+
+        System.out.println("Answer = " + answer);
+
+        URL postUrl = new URL(baseUrl + "/submit");
+
+        HttpURLConnection postConn = (HttpURLConnection) postUrl.openConnection();
+        postConn.setRequestMethod("POST");
+        postConn.setRequestProperty("Content-Type", "application/json");
+        postConn.setDoOutput(true);
+
+        String jsonPost =
+                "{"
+                + "\"studentCode\":\"" + studentCode + "\","
+                + "\"qCode\":\"" + qCode + "\","
+                + "\"requestId\":\"" + requestId + "\","
+                + "\"answer\":\"" + answer + "\""
+                + "}";
+
+        OutputStream os = postConn.getOutputStream();
+        os.write(jsonPost.getBytes("UTF-8"));
+        os.flush();
+        os.close();
+
+        BufferedReader resultReader =
+                new BufferedReader(new InputStreamReader(postConn.getInputStream()));
+
+        StringBuilder result = new StringBuilder();
+
+        while ((line = resultReader.readLine()) != null) {
+            result.append(line);
+        }
+
+        resultReader.close();
+
+        System.out.println(result.toString());
+    }
+}
